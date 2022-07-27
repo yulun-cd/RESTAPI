31cedf6e467d1572c24f4454ca3c8076fe3f7450
yulun-cd <34034066+yulun-cd@users.noreply.github.com>
2022-07-27 14:11:33 +0100

    add TokenRefresh

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 import re
+from tokenize import Token
 
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
-from resources.user import UserRegister, User, UserLogin
+from resources.user import UserRegister, User, UserLogin, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
@@ -40,6 +41,7 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
+api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
     db.init_app(app)
--- resources/user.py
+++ resources/user.py
@@ -1,4 +1,4 @@
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required
 from flask_restful import Resource, reqparse
 
 from models.user import UserModel
@@ -64,3 +64,11 @@ class UserLogin(Resource):
             }, 200
             
         return {'message': 'Invalid credentials'}, 401
+    
+
+class TokenRefresh(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        curr_user = get_jwt_identity()
+        new_token = create_access_token(identity=curr_user, fresh=False)
+        return {'access_token': new_token}, 200
